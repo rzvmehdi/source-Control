e6bf3e4acff24da789bac435660bddc90031f4f8
Mahdi <rzvmehdi@yahoo.com>
2020-07-12 17:10:50 +0100

    addNumbers method

--- source Control/ViewController.swift
+++ source Control/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    /**
+     Adds two numbers together  and returns the result.
+     - parameter num1: The first number
+     - parameter num2: the second number
+     - returns: The sum up of two numbers
+     
+     */
+    func addNum(num1: Int, num2: Int) -> Int{
+        return num1 + num2
+    }
+    
 
     override var representedObject: Any? {
         didSet {
