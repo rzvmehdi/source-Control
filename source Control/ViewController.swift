1581197ba7e3c9a9ec89b5288d16ca7c69170386
Mahdi <rzvmehdi@yahoo.com>
2020-07-12 17:03:32 +0100

    Initial Commit

---
+++ source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  source Control
+//
+//  Created by Mahdi Razavi on 12/07/2020.
+//  Copyright © 2020 Mahdi Razavi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
